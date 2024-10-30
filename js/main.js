dd56612b820ee8f3fe827d20f35c35f2c1361ecd
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2024-10-30 16:48:38 -0300

    Requisição post para enviar pensamentos para a api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 import ui from './ui.js'
+import api from './api.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   ui.renderizarPensamentos()
+
+  const formularioPensamento = document.getElementById('pensamento-form')
+  formularioPensamento.addEventListener("submit", manipularSubmitFormulario)
 })
+async function manipularSubmitFormulario(event) {
+  event.preventDefault()
+  const id = document.getElementById('pensamento-id').value
+  const conteudo = document.getElementById('pensamento-conteudo').value
+  const aurotia = document.getElementById('pensamento-autoria').value
+
+  try {
+    await api.salvarPensamentos({ conteudo, autoria })
+    ui.renderizarPensamentos()
+  } catch {
+    alert("Erro ao salvar os pensamentos")
+  }
+}
