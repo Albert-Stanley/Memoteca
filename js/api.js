45ef9d84bf9ea8b2395955c160d313c2e1036492
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2024-11-02 10:23:18 -0300

    Criando requisição put para alteração de dados

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@ const api = {
       return await response.json()
     }
     catch (error) {
-      alert('Erro')
+      alert('Erro ao buscar pensamentos!')
       throw error
     }
   },
@@ -21,7 +21,33 @@ const api = {
       return await response.json()
     }
     catch (error) {
-      alert('Erro')
+      alert('Erro ao salvar pensamento!')
+      throw error
+    }
+  },
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao buscar pensamento por id!')
+      throw error
+    }
+  },
+  async editarPensamentos(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao editar pensamento!')
       throw error
     }
   }
